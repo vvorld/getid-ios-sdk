f72de7962a9a7fa89181ab9aa9978268e3ce4acc
Mikhail Akopov <mikhail.akopov@getid.ee>
2020-10-28 09:47:27 +0200

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "GetID",
+    products: [
+        .library(
+            name: "GetID",
+            targets: ["GetID"]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(name: "GetID",
+                      url: "https://github.com/vvorld/getid-ios-sdk/releases/download/x.y.z/GetID.xcframework.zip",
+                      checksum: "...")
+    ]
+)
