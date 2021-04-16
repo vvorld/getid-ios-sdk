f47b39a3e600f279718788ffe32c11513b41682c
Mikhail Akopov <mikhail.akopov@getid.ee>
2021-04-16 13:23:49 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     dependencies: [],
     targets: [
         .binaryTarget(name: "GetID",
-                      url: "https://github.com/vvorld/getid-ios-sdk/releases/download/2.0.0/GetID.xcframework.zip",
-                      checksum: "5f7563fbf51326d206990c3b48a0e046cfaba826713853cabc54deddbed01c0f")
+                      url: "https://github.com/vvorld/getid-ios-sdk/releases/download/2.0.1/GetID.xcframework.zip",
+                      checksum: "641c172a8467bd34de5f37c07c7d7117cd99a705ce504c0448d3872fc92d6cde")
     ]
 )
