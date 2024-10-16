f11e47a753bc7be61e382f29634de922c34d12a5
Per Stråkendal <per.strakendal@checkin.com>
2024-10-16 15:04:23 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,11 +9,14 @@ let package = Package(
   products: [
     .library(
       name: "GetID",
-      targets: ["GetID"])
+      targets: ["GetID", "_GetIDStub"])
   ],
   dependencies: [
-    .package(name: "Lottie", url: "https://github.com/airbnb/lottie-spm.git", .upToNextMajor(from: "4.3.3")),
-    .package(name: "RecaptchaEnterprise",
+    .package(
+      name: "Lottie", url: "https://github.com/airbnb/lottie-spm.git", .upToNextMajor(from: "4.3.3")
+    ),
+    .package(
+      name: "RecaptchaEnterprise",
       url: "https://github.com/GoogleCloudPlatform/recaptcha-enterprise-mobile-sdk",
       .upToNextMajor(from: "18.3.0")),
   ],
@@ -21,6 +24,10 @@ let package = Package(
     .binaryTarget(
       name: "GetID",
       url: "https://github.com/vvorld/getid-ios-sdk/releases/download/3.2.3/GetID.xcframework.zip",
-      checksum: "3ed19443f995d14b5cce64d7b932c33732aea879fc3b35aeb3f81449fc25c4a6")
+      checksum: "3ed19443f995d14b5cce64d7b932c33732aea879fc3b35aeb3f81449fc25c4a6"
+    ),
+    .target(
+      name: "_GetIDStub",
+      dependencies: ["GetID", "Lottie", "RecaptchaEnterprise"]),
   ]
 )
