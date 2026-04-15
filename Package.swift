997660605625df15ad397d27a1aa9a0c8f621e5a
Mohcine Belarrem <belarrem.mohcine@gmail.com>
2026-04-15 13:23:47 +0100

    Adds SwiftDraw

--- Package.swift
+++ Package.swift
@@ -16,6 +16,9 @@ let package = Package(
         name: "RecaptchaEnterprise",
         url: "https://github.com/GoogleCloudPlatform/recaptcha-enterprise-mobile-sdk",
         .upToNextMajor(from: "18.7.0")),
+    .package(
+        url: "https://github.com/swhitty/SwiftDraw.git",
+        .upToNextMajor(from: "0.26.0")),
   ],
   targets: [
     .binaryTarget(
@@ -24,6 +27,6 @@ let package = Package(
       checksum: "29459fb905b8380dcaff8823cbf3b44f3b77fdd056389ad27c2f8cb46425e229"),
     .target(
       name: "_GetIDStub",
-      dependencies: ["GetID", "RecaptchaEnterprise"]),
+      dependencies: ["GetID", "RecaptchaEnterprise", "SwiftDraw"]),
   ]
 )
